8343e83df3137add6282355ad26cea66c784569a
cribbee <cribbee66@gmail.com>
2018-10-17 14:34:27 +0800

    scope:spring-service-test and logger body: to configure log and do the test for Service

--- src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -85,7 +85,7 @@ public class SeckillServiceImpl implements SeckillService {
      *
      */
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
-        if(md5 == null || md5.equals(getMd5(seckillId))){
+        if(md5 == null || !md5.equals(getMd5(seckillId))){
             throw  new SeckillException("seckill data rewrite");
         }
         //执行秒杀逻辑：减库存 + 记录秒杀行为
